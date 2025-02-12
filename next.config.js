bc20cdccb5dd6b99e0e0c81c398fa8161770e8b6
mammolastan <mammolastan@hotmail.com>
2025-02-12 13:28:05 -0500

    deployment strategy shift

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone", // Optimizes the output for production
+  poweredByHeader: false,
+  generateEtags: true,
+  compress: true,
+  webpack: (config, { dev, isServer }) => {
+    // Production optimizations
+    if (!dev) {
+      config.optimization = {
+        ...config.optimization,
+        nodeEnv: "production",
+        minimize: true,
+        mergeDuplicateChunks: true,
+        removeEmptyChunks: true,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
