1f117faaea8e06c8d465226227fe0e20e7cd1b17
mammolastan <mammolastan@hotmail.com>
2025-04-27 09:38:23 -0400

    Update TooltipContent max-width for better responsiveness

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -29,7 +29,7 @@ const TooltipContent = React.forwardRef<
         ref={ref}
         sideOffset={sideOffset}
         className={cn(
-            "z-50 overflow-hidden rounded-md bg-gray-900 px-3 py-1.5 text-xs text-gray-50 max-w-[70%] whitespace-normal break-words",
+            "z-50 overflow-hidden rounded-md bg-gray-900 px-3 py-1.5 text-xs text-gray-50 max-w-[92vw] md:max-w-[70%] whitespace-normal break-words",
             className
         )}
         {...props}
